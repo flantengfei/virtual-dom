51a6919b5801ae9c4cbbc57b10a94cb63a072fab
Fei Teng <flan_tengfei@hotmail.com>
2019-02-18 22:18:43 -0600

    add createElement function

--- index.jsx
+++ index.jsx
@@ -1,16 +1,31 @@
 const view = count => {
-  return <div className="container">123 <span>My Text</span></div>
+  return <div className="container">123
+  <span>My Text</span>
+    <div className='div1'>div1</div>
+    <div className='div2'>div2</div>
+    <div className='div3'>div3</div>
+  </div>
 }
 
 const flatten = arr => [].concat.apply([], arr)
 
 const h = (type, props, ...children) => {
   props = props || {}
-  return {type, props, children: flatten(children)}
+  return { type, props, children }
 }
 
-const createElement = node =>
-  (typeof node === 'string') ? document.createTextNode(node) : document.createElement(node.type)
+const createElement = node => {
+  if (typeof node === 'string') {
+    return document.createTextNode(node)
+  }
+  const el = document.createElement(node.type)
+  node.children.map(createElement).forEach(
+    element => {
+      el.appendChild(element)
+    }
+  )
+  return el
+}
 
 
 const render = el => {
